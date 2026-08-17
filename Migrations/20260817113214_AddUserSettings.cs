06ca1381156ca62141664b7585902ee4fbd3892d
Weso <mohamed.hosam.eldeeen@gmail.com>
2026-08-17 14:33:47 +0300

    AddUserSettings Migration and Updating Database

---
+++ Migrations/20260817113214_AddUserSettings.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecruitmentApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarUrl",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IncognitoMode",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ProfileVisible",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PushNotifications",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SubscriptionPlan",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarUrl",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IncognitoMode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileVisible",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PushNotifications",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SubscriptionPlan",
+                table: "Users");
+        }
+    }
+}
